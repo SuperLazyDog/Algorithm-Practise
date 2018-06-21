af105d628b0681c12a045c3e5dd6516741746baa
SuperLazyDog <jyoitatsu@foxmail.com>
2018-06-21 23:12:20 +0900

    Done-step: [切割头文件]

--- books/oreilly/s4 stack/lecture/stack.c
+++ books/oreilly/s4 stack/lecture/stack.c
@@ -1,23 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-// 栈的基本实现
-struct listNode {
-  int data;
-  struct listNode *next;
-}
-
-struct stack {
-  int count;
-  struct listnode *head;
-}
-
-struct stack *createStack(void); // 创建stack 0(1)
-void push(struct stack *stack, int data); // push数据 O(1)
-int isEmptyStack(struct stack *stack); // 检验是否为空 O(1)
-int pop(struct stack *stack); // pop数据 O(1)
-int top(struct stack *stack); // 查看第一个元素 0(1)
-void deleteStack(struct stack *stack); // 删除整个栈 0(n)
 
 struct stack *createStack() { // 创建stack
   struct stack *temp = (struct stack *)malloc(sizeof(struct stack));
