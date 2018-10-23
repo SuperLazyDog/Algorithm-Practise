e727504bb9f2eea45d2614ef1717d165d5f46c8e
SuperLazyDog <jyoitatsu@foxmail.com>
2018-10-23 10:14:55 +0900

    Done-step: [20181023当堂作业]

---
+++ school/programming/20181023/1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  int n, i, j, k, count=0;
+
+  scanf("%d", &n);
+  scanf("%d", &t);
+  for (i = 1; i <= 6; i++) {
+    for(j = 1; j <= 6; j++) {
+      for(k=1; k<=6; k++) {
+        if (i+j+k == n) {
+          count++;
+        }
+      }
+    }
+  }
+  printf("%d\n", count);
+  return 0;
+}
