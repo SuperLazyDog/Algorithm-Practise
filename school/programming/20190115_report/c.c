b175ded3f68e1538e4e396ba40a59953dd2d49bd
SuperLazyDog <jyoitatsu@foxmail.com>
2019-01-11 15:23:31 +0900

    Done-step: [学校c报告]

--- school/programming/20190115_report/c.c
+++ school/programming/20190115_report/c.c
@@ -1,44 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// abOcOOdOOO
-//   1 23 456
-//   111
-//   103
-//   012
-//   021
-//   003
+#include <math.h>
+
+#define LIMIT 0.00000000001
+typedef struct o {
+  char num[4], exp[7];
+  int t[4];
+} NE;
+void solution(NE s, int n);
 double caculate(double a, double b, char type);
 double expCaculate(char str[7]);
 void showStandartExp(char str[7]);
 int main(int argc, char const *argv[]) {
-  char str[4], exp[7];
-  int temp[10];
-  // do {
-    // int j  = 0;
-    // printf("num: ");
-    // scanf("%s", str);
-    // for (int i = 0; i < 10; i++) {
-    //   temp[i] = -9;
-    // }
-    // temp[0] = str[0]-'0';
-    // temp[1] = str[1]-'0';
-    // temp[3] = str[2]-'0';
-    // temp[6] = str[3]-'0';
-    // temp[7] = temp[8] = temp[9] = -1;
-    // for (int i = 0; i < 10; i++) {
-    //   if (temp[i] == -9) {
-    //     continue;
-    //   }
-    //   exp[j++] = temp[i]+'0';
-    // }
-    // printf("%s\n", exp);
-    // printf("result: %f\n", expCaculate("1234+++"));
-    // puts("");
-  // } while(strcmp(str, "quit"));
-  printf("result: %f\n", expCaculate("1234+++"));
-  showStandartExp("1234+++");
+  char str[4], over[] = "quit";
+  do {
+    NE s;
+    for (size_t i = 0; i < 4; i++) {
+      s.t[i] = 0;
+    }
+    printf("num(abcd): ");
+    scanf("%s", s.num);
+    solution(s, 0);
+  } while(strcmp(str, over));
   return 0;
+}
+
+void solution(NE s, int n) {
+  // abOcOOdOOO
+  //   O OO OOO
+  //   111 abXcXdX
+  //   102 abXcdXX
+  //   012 abcXdXX
+  //   021 abcXXdX
+  //   003 abcdXXX
+  if (n >= 7) {
+    char strs[5][8], temp[2];
+    for (int i = 0; i < 5; i++) {
+      for (int j = 0; j <= 7; j++) {
+        strs[i][j] = s.exp[j];
+      }
+    }
+    temp[0] = s.exp[4]; temp[1] = s.exp[5];
+    strs[1][5] = strs[1][3];
+    strs[1][3] = temp[0];
+    strs[1][4] = temp[1];
+
+    strs[2][4] = strs[2][3];
+    strs[2][3] = temp[0];
+
+    strs[3][4] = strs[3][3];
+    strs[3][3] = strs[3][2];
+    strs[3][2] = temp[0];
+
+    strs[4][5] = strs[4][3];
+    strs[4][3] = strs[4][2];
+    strs[4][4] = temp[1];
+    strs[4][2] = temp[0];
+
+    for (size_t i = 0; i < 5; i++) {
+      if (fabs(expCaculate(strs[i])-10.)<LIMIT) {
+        showStandartExp(strs[i]);
+      }
+    }
+  } else if (n >= 4) {
+    char op[4] = {'+', '-', '*', '/'};
+    for (size_t i = 0; i < 4; i++) {
+      s.exp[n] = op[i];
+      solution(s, n+1);
+    }
+  } else {
+    for (int i = 0; i < 4; i++) {
+      if (!s.t[i]) {
+        s.t[i] = 1;
+        s.exp[n] = s.num[i];
+        solution(s, n+1);
+        s.t[i] = 0;
+      }
+    }
+  }
 }
 
 double caculate(double a, double b, char type) {
@@ -56,53 +96,47 @@ double caculate(double a, double b, char type) {
 }
 
 double expCaculate(char str[7]) {
-  double numStack[4];
+  double numS[4];
   int numCount, i, temp;
   numCount = i = 0;
   while (str[i]) {
     temp = str[i]-'0';
     if (temp >= 0 && temp < 10) {
-      numStack[numCount++] = temp;
+      numS[numCount++] = temp;
     } else {
-      int b = numStack[--numCount], a = numStack[--numCount];
-      numStack[numCount++] = caculate(a, b, str[i]);
+      double b = numS[--numCount], a = numS[--numCount];
+      numS[numCount++] = caculate(a, b, str[i]);
     }
     i++;
-    // for (int i = 0; i < numCount; i++) {
-    //   printf("stack[%d]: %f\n", i, numStack[i]);
-    // }
-    // puts("");
   }
-  return numStack[--numCount];
+  return numS[--numCount];
 }
 
 void showStandartExp(char str[7]) {
-  char strStack[4][32];
+  char strS[4][32];
   for (int i = 0; i < 4; i++) {
-    strcpy(strStack[i], "");
+    strcpy(strS[i], "");
   }
   int count, i, temp;
   count = i = 0;
   while (str[i]) {
     temp = str[i]-'0';
     if (temp >= 0 && temp < 10) {
-      char temp[32];
-      // sscanf(temp, "%c", str[i]);
-      temp[0] = str[i];
-      printf("temp: %s\n", temp);
-      strcpy(strStack[count++], temp);
+      char ctemp[32];
+      sprintf(ctemp, "%c", str[i]);
+      strcpy(strS[count++], ctemp);
     } else {
-      char b[32], a[32], temp[32];
-      strcpy(b, strStack[--count]);
-      strcpy(a, strStack[--count]);
-      // sscanf(temp, "(%s+%s)", a, b);
-      printf("a: %s, b: %s\n", a, b);
-      strcpy(strStack[count++], temp);
+      char b[16], a[16], temp[32];
+      strcpy(b, strS[--count]);
+      strcpy(a, strS[--count]);
+      if (i == 6) {
+        sprintf(temp, "%s%c%s", a, str[i], b);
+      } else {
+        sprintf(temp, "(%s%c%s)", a, str[i], b);
+      }
+      strcpy(strS[count++], temp);
     }
     i++;
-    for (int i = 0; i < count; i++) {
-      printf("%d: %s\n", i, strStack[i]);
-    }
-    puts("");
   }
+  printf("%s\n", strS[0]);
 }
